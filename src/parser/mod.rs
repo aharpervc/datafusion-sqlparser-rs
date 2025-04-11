d5d376e73764872124a94bb68e6afe8961e34345
Andrew Harper <andrew.harper@veracross.com>
2025-04-11 15:38:17 -0400

    Add `OR ALTER` support for `CREATE TRIGGER`

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -4556,9 +4556,9 @@ impl<'a> Parser<'a> {
         } else if self.parse_keyword(Keyword::FUNCTION) {
             self.parse_create_function(or_alter, or_replace, temporary)
         } else if self.parse_keyword(Keyword::TRIGGER) {
-            self.parse_create_trigger(or_replace, false)
+            self.parse_create_trigger(or_alter, or_replace, false)
         } else if self.parse_keywords(&[Keyword::CONSTRAINT, Keyword::TRIGGER]) {
-            self.parse_create_trigger(or_replace, true)
+            self.parse_create_trigger(or_alter, or_replace, true)
         } else if self.parse_keyword(Keyword::MACRO) {
             self.parse_create_macro(or_replace, temporary)
         } else if self.parse_keyword(Keyword::SECRET) {
@@ -5256,6 +5256,7 @@ impl<'a> Parser<'a> {
 
     pub fn parse_create_trigger(
         &mut self,
+        or_alter: bool,
         or_replace: bool,
         is_constraint: bool,
     ) -> Result<Statement, ParserError> {
@@ -5265,7 +5266,7 @@ impl<'a> Parser<'a> {
         }
 
         if dialect_of!(self is MsSqlDialect) {
-            return self.parse_mssql_create_trigger(or_replace, is_constraint);
+            return self.parse_mssql_create_trigger(or_alter, or_replace, is_constraint);
         }
 
         let name = self.parse_object_name(false)?;
@@ -5309,6 +5310,7 @@ impl<'a> Parser<'a> {
         let exec_body = self.parse_trigger_exec_body()?;
 
         Ok(Statement::CreateTrigger {
+            or_alter,
             or_replace,
             is_constraint,
             name,
@@ -5331,6 +5333,7 @@ impl<'a> Parser<'a> {
     /// [MsSql]: https://learn.microsoft.com/en-us/sql/t-sql/statements/create-trigger-transact-sql
     pub fn parse_mssql_create_trigger(
         &mut self,
+        or_alter: bool,
         or_replace: bool,
         is_constraint: bool,
     ) -> Result<Statement, ParserError> {
@@ -5361,6 +5364,7 @@ impl<'a> Parser<'a> {
         };
 
         Ok(Statement::CreateTrigger {
+            or_alter,
             or_replace,
             is_constraint,
             name,
--- tests/sqlparser_mssql.rs
+++ tests/sqlparser_mssql.rs
@@ -2128,7 +2128,7 @@ fn parse_mssql_merge_with_output() {
 #[test]
 fn parse_create_trigger() {
     let create_trigger = "\
-        CREATE TRIGGER reminder1 \
+        CREATE OR ALTER TRIGGER reminder1 \
         ON Sales.Customer \
         AFTER INSERT, UPDATE \
         AS RAISERROR('Notify Customer Relations', 16, 10);\
@@ -2137,6 +2137,7 @@ fn parse_create_trigger() {
     assert_eq!(
         create_stmt,
         Statement::CreateTrigger {
+            or_alter: true,
             or_replace: false,
             is_constraint: false,
             name: ObjectName::from(vec![Ident::new("reminder1")]),
