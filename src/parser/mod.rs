e5d6791c9c22382374d8c2a1bb7bdea2a80e5980
Andrew Harper <andrew.harper@veracross.com>
2025-07-21 17:58:38 -0400

    Restore former "end of statement" logic

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -526,7 +526,12 @@ impl<'a> Parser<'a> {
 
             match &self.peek_token_ref().token {
                 Token::EOF => break,
-
+                // end of statement
+                Token::Word(word) => {
+                    if expecting_statement_delimiter && word.keyword == Keyword::END {
+                        break;
+                    }
+                }
                 // don't expect a semicolon statement delimiter after a newline when not otherwise required
                 Token::Whitespace(Whitespace::Newline) => {
                     if !self.options.require_semicolon_stmt_delimiter {
